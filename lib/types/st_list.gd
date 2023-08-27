95e228cdf5c5f3cb83dfae1de216e1e31fe7d6fe
wanpst <wanderingpistol@gmail.com>
2023-08-26 23:08:59 -0300

    rename StList `value` to `elements`

--- lib/types/st_list.gd
+++ lib/types/st_list.gd
@@ -1,10 +1,10 @@
 class_name StList
 extends StType
 
-var value: Array[StType]
+var elements: Array[StType]
 
-func _init(p_value: Array[StType] = []) -> void:
-	value = p_value
+func _init(p_elements: Array[StType] = []) -> void:
+	elements = p_elements
 
 func _to_string() -> String:
-	return "(" + " ".join(value.map(StType.pr_str)) + ")"
+	return "(" + " ".join(elements.map(StType.pr_str)) + ")"
