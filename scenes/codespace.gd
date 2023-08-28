f4f72bea413a85a944d7e3ac483428f891f55216
wanpst <wanderingpistol@gmail.com>
2023-08-28 11:25:06 -0300

    docs: remove large comment explaining the token regex pattern
    surely in 2023 i can expect people to know how to figure out regex?

--- scenes/codespace.gd
+++ scenes/codespace.gd
@@ -24,27 +24,6 @@ class Reader:
 
 
 func _ready() -> void:
-	# Pilfered directly from https://github.com/kanaka/mal. I had to
-	#
-	# - [\s,]*: Matches any number of whitespaces or commas. This is not captured
-	#   so it will be ignored and not tokenized.
-	#
-	# - ~@: Captures the special two-characters ~@ (tokenized).
-	#
-	# - [\[\]{}()'`~^@]: Captures any special single character, one of
-	#   []{}()'`~^@ (tokenized).
-	#
-	# - "(?:\\.|[^\\"])*"?: Starts capturing at a double-quote and stops at the
-	#   next double-quote unless it was preceded by a backslash in which case it
-	#   includes it until the next double-quote (tokenized). It will also
-	#   match unbalanced strings (no ending double-quote) which should be
-	#   reported as an error.
-	#
-	# - ;.*: Captures any sequence of characters starting with ; (tokenized).
-	#
-	# - [^\s\[\]{}('"`,;)]*: Captures a sequence of zero or more non special
-	#   characters (e.g. symbols, numbers, "true", "false", and "nil") and is sort
-	#   of the inverse of the one above that captures special characters (tokenized).
 	token_regex.compile("[\\s ,]*(~@|[\\[\\]{}()'`~@]|\"(?:[\\\\].|[^\\\\\"])*\"?|;.*|[^\\s \\[\\]{}()'\"`~@,;]*)")
 	string_regex.compile("^\"((?:[\\\\].|[^\\\\\"])*)\"$")
 
