47742510a1800073f4acfed4a96c7f00ab30304a
wanpst <wanderingpistol@gmail.com>
2023-08-28 04:04:29 -0300

    fix: remove redundant type check in StType.pr_str()
    there's already a type hint to do that!

--- lib/types/st_type.gd
+++ lib/types/st_type.gd
@@ -2,8 +2,4 @@ class_name StType
 extends RefCounted
 
 static func pr_str(input: StType) -> String:
-	if input is StType:
-		return input.to_string()
-	else:
-		push_error("Unhandled type")
-		return ""
+	return str(input)
