c54867295d5fc4463f5ba584dda123aad2e6c7ac
wanpst <wanderingpistol@gmail.com>
2023-08-31 01:24:23 -0300

    fix: move special string pretty-printing to a StString method

--- lib/types/st_string.gd
+++ lib/types/st_string.gd
@@ -6,3 +6,14 @@ var value: String
 
 func _init(p_value: String = "") -> void:
 	value = p_value
+
+
+func pretty_to_string(print_readably := false) -> String:
+	if value.begins_with("\u029e"):
+		return ":" + value.substr(1)
+
+	if print_readably:
+		# NOTE: c_escape() will escape single quotes; this is close enough!
+		return '\"' + value.json_escape() + '\"'
+	else:
+		return '\"' + value + '\"'
--- lib/types/st_type.gd
+++ lib/types/st_type.gd
@@ -3,16 +3,7 @@ extends RefCounted
 
 
 static func pr_str(input: StType, print_readably := false) -> String:
-	# FIXME: ideally, this would be in StString's _to_string. but Godot says
-	# it takes no arguments, and making a replacement function to use for
-	# every single StType just to move one block of code seems even worse
 	if input is StString:
-		if not input.value.is_empty() and input.value[0] == "\u029e":
-			return ":" + input.value.substr(1)
-		if print_readably:
-			# NOTE: c_escape() will escape single quotes; this is close enough!
-			return '\"' + input.value.json_escape() + '\"'
-		else:
-			return '\"' + input.value + '\"'
+		return input.pretty_to_string(print_readably)
 
 	return str(input)
